59c276c6d61ec06c8911457653f78b9cf87d65bf
OriOfek1 <oriofek1@gmail.com>
2024-03-21 23:05:30 +0200

    Defaulting to 8080 if PORT environment variable is not set

--- app.py
+++ app.py
@@ -229,4 +229,5 @@ async def dashboard(request: Request, uuid: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.getenv("PORT", "8080"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
